7f21a94a33184cc31891213301d4156f974a1c6c
denfort50 <denfort50@yandex.ru>
2023-02-24 12:45:44 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accident/controller/IndexController.java
+++ src/main/java/ru/job4j/accident/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accident.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,7 @@ public class IndexController {
     public String index(Model model) {
         List<Accident> accidents = accidentService.findAll();
         model.addAttribute("allAccidents", accidents);
-        model.addAttribute("user", "Denis Kalchenko");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "index";
     }
 }
